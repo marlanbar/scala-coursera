4a4af9595443f8b3b9c8699d927479422c4e7c8b
marlanbar <martinlangberg@gmail.com>
2014-01-01 02:53:39 -0300

    example-assigment terminado

--- example-assignment/src/test/scala/example/ListsSuite.scala
+++ example-assignment/src/test/scala/example/ListsSuite.scala
@@ -47,7 +47,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 != 3) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +72,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 != 3) // Fix me, please!
   }
 
 
@@ -115,10 +115,18 @@ class ListsSuite extends FunSuite {
    * every tested aspect of a method.
    */
   test("sum of a few numbers") {
-    assert(sum(List(1,2,0)) === 3)
+    assert(sum(List(1,2,0, -5)) === -2)
+    assert(sum(List(0)) === 0)
   }
   
   test("max of a few numbers") {
-    assert(max(List(3, 7, 2)) === 7)
+    assert(max(List(7, 3, 7, 2)) === 7)
+    assert(max(List(1)) === 1)
+  }
+  
+  test("max throws an exception if its argument is empty") {
+    intercept[java.util.NoSuchElementException] {
+      max(List())
+    }
   }
 }
